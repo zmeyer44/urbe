8512b1d779062cb28914697e7f84b8c0c5860342
zmeyer44 <zmmeyer44@gmail.com>
2024-12-13 12:50:51 -0500

    more testing tests

--- apps/www/__tests__/feed.test.tsx
+++ apps/www/__tests__/feed.test.tsx
@@ -1,6 +1,6 @@
-import Page from '@/app/(main)/page';
 import { render, screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
+import Page from '../app/(main)/page';
 
 test('Sign In Page', () => {
   render(<Page />);
